5e04c4fea4da4c690712bdcc7b6b4b39ee3afb5c
Pierre Baillargeon <pierrebai@hotmail.com>
2020-10-03 20:48:30 -0400

    Added drawing canvas that calls any function to draw.

---
+++ ui_qt/include/dak/ui/qt/function_drawing_canvas.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#ifndef DAK_UI_QT_DRAWING_CANVAS_H
+#define DAK_UI_QT_DRAWING_CANVAS_H
+
+#include <dak/ui/qt/canvas.h>
+
+#include <dak/ui/qt/painter_transformable.h>
+
+#include <functional>
+
+namespace dak::ui::qt
+{
+
+   ////////////////////////////////////////////////////////////////////////////
+   //
+   // A widget canvas working with a transformable painter drawing.
+
+   class function_drawing_canvas_t : public drawing_canvas_t
+   {
+   public:
+      using draw_func_t = std::function<void(drawing_t& drw)>;
+
+      // Create a canvas with the given parent widget and draw function.
+      function_drawing_canvas_t(QWidget* parent, draw_func_t a_func)
+         : drawing_canvas_t(parent), my_func(a_func) {}
+
+   protected:
+      // Paint using the prepared drawing by calling the function.
+      void draw(drawing_t& drw) override
+      {
+         if (!my_func)
+            return;
+
+         my_func(drw);
+      }
+
+      draw_func_t my_func;
+   };
+}
+
+#endif
+
+// vim: sw=3 : sts=3 : et : sta : 
